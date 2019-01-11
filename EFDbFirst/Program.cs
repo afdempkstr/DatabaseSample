c06d7c70008ede00079dcd1045ac0644bd8269cd
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-01-11 20:42:35 +0200

    Predicates, functions, et al

--- EFDbFirst/Program.cs
+++ EFDbFirst/Program.cs
@@ -14,10 +14,66 @@ namespace EFDbFirst
             {
                 restaurant.Print();
             }
+            Console.WriteLine("-------------------");
+
+            foreach (var restaurant in FilterRestaurants(Souvlakia))
+            {
+                restaurant.Print();
+            }
+            Console.WriteLine("-------------------");
+
+            foreach (var restaurant in FilterRestaurants(r => Souvlakia(r)))
+            {
+                restaurant.Print();
+            }
+            Console.WriteLine("-------------------");
+
+            foreach (var restaurant in FilterRestaurants(r => r.IsOpen))
+            {
+                restaurant.Print();
+            }
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Please give the name to search for:");
+            var name = Console.ReadLine();
+
+            foreach (var restaurant in FilterRestaurants(r => r.Name.Contains(name)))
+            {
+                restaurant.Print();
+            }
 
             Console.ReadKey();
         }
 
+        static IEnumerable<Restaurant> GetSouvlakoRestaurants()
+        {
+            using (var db = new RestaurantModel())
+            {
+                //return db.Restaurants.Include("Cuisine")
+                //    .Where(Souvlakia)
+                //    .ToList();
+
+                return db.Restaurants.Include("Cuisine")
+                        .Where(restaurant => Souvlakia(restaurant))
+                        .ToList();
+            }
+        }
+
+        static IEnumerable<Restaurant> FilterRestaurants(Func<Restaurant, bool> check)
+        {
+            using (var db = new RestaurantModel())
+            {
+                return db.Restaurants.Include("Cuisine")
+                        .Where(check)
+                        .ToList();
+            }
+        }
+
+
+        static bool Souvlakia(Restaurant restaurant)
+        {
+            return restaurant.Cuisine?.Description == "Σουβλάκι";
+        }
        
         static IEnumerable<Restaurant> GetRestaurants()
         {
